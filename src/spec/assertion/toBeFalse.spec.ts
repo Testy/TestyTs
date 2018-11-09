7e8f23c5518a5901ef8fa44727c754cfc8b7e03d
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2018-11-09 11:41:03 -0500

    Refactored toBeFalse tests to use testcases

--- src/spec/assertion/toBeFalse.spec.ts
+++ src/spec/assertion/toBeFalse.spec.ts
@@ -1,53 +1,44 @@
 import { test, testSuite, expect } from '../../testy';
+import { TestCase } from '../../lib/testCase';
 
 @testSuite('Expect ToBeFalse Test Suite')
 class ExpectToBeFalseTestSuite {
-    
-    @test('false to be false')
+
+    @test('false to be false, should succeed')
     private falseToBeFalse() {
         expect.toBeFalse(false);
     }
 
-    @test('true to be false to fail')
-    private trueToBeFalseFails() {
-        expect.toThrow(() => {
-            expect.toBeFalse(true);
-        });
-    }
-
-    @test('undefined to be false to fail')
-    private undefinedToBeFalseFails() {
-        expect.toThrow(() => {
-            expect.toBeFalse(undefined);
-        });
-    }
-
-    @test('null to be false to fail')
-    private nullToBeFalseFails() {
-        expect.toThrow(() => {
-            expect.toBeFalse(null);
-        });
-    }
-
-    @test('true not to be false')
-    private trueNotToBeFalse() {
-        expect.not.toBeFalse(true);
-    }
-
-    @test('false not to be false to fail')
-    private falseNotToBeFalse() {
+    @test('false not to be false, should fail')
+    private falseNotToBeFalseFail() {
         expect.toThrow(() => {
             expect.not.toBeFalse(false);
         });
     }
 
-    @test('undefined not to be false')
-    private undefinedNotToBeFalse() {
-        expect.not.toBeFalse(undefined);
+    @test('To be false, should fail', [
+        new TestCase('true', true),
+        new TestCase('undefined', undefined),
+        new TestCase('null', null),
+        new TestCase('empty array', []),
+        new TestCase('empty object', {}),
+        new TestCase('NaN', NaN)
+    ])
+    private toBeFalseFail(arg) {
+        expect.toThrow(() => {
+            expect.toBeFalse(arg);
+        });
     }
 
-    @test('null not to be false')
-    private nullNotToBeFalse() {
-        expect.not.toBeFalse(null);
+    @test('Not to be false, should succeed', [
+        new TestCase('true', true),
+        new TestCase('undefined', undefined),
+        new TestCase('null', null),
+        new TestCase('empty array', []),
+        new TestCase('empty object', {}),
+        new TestCase('NaN', NaN)
+    ])
+    private notToBeFalseSuccess(arg) {
+        expect.not.toBeFalse(arg);
     }
 }
