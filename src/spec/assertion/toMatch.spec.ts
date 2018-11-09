d1990da365bcdc6473cfac6a447a0fa67df28e71
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2018-11-09 12:04:49 -0500

    Implemented toMatch tests

---
+++ src/spec/assertion/toMatch.spec.ts
@@ -0,0 +1,28 @@
+import { test, testSuite, expect } from '../../testy';
+
+@testSuite('Expect ToMatch Tests')
+class ExpectToMatch {
+    @test(`'hello' to match /[a-z]/, should succeed`)
+    private helloToMatchSuccess() {
+        expect.toMatch('hello', /[a-z]/);
+    }
+
+    @test(`'hello' to match /[A-Z]/, should fail`)
+    private helloToMatchFail() {
+        expect.toThrow(() => {
+            expect.toMatch('hello', /[A-Z]/);
+        });
+    }
+
+    @test(`'hello' not to match /[a-z]/, should fail`)
+    private helloNotToMatchFail() {
+        expect.toThrow(() => {
+            expect.not.toMatch('hello', /[a-z]/);
+        });
+    }
+
+    @test(`'hello' not to match /[A-Z]/, should fail`)
+    private helloNotToMatchSuccess() {
+        expect.not.toMatch('hello', /[A-Z]/);
+    }
+}
