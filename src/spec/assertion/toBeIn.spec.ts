bebbbf5a9394d3cbb5ffa49ebcb805a5b51333ff
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2018-11-09 12:14:31 -0500

    Implemented toBeIn tests

---
+++ src/spec/assertion/toBeIn.spec.ts
@@ -0,0 +1,28 @@
+import { test, testSuite, expect } from '../../testy';
+
+@testSuite('Expect ToBeIn Tests')
+class ExpectToMatch {
+    @test(`'a' to be in, should succeed`)
+    private aToBeInSuccess() {
+        expect.toBeIn('a', ['a', 'b', 'c']);
+    }
+
+    @test(`'a' to be in, should fail`)
+    private aToBeInFail() {
+        expect.toThrow(() => {
+            expect.toBeIn('a', ['b', 'c', 'd']);
+        });
+    }
+
+    @test(`'a' not to be in, should fail`)
+    private aNotToBeInFail() {
+        expect.toThrow(() => {
+            expect.not.toBeIn('a', ['a', 'b', 'c']);
+        });
+    }
+
+    @test(`'a' not to be in, should succeed`)
+    private aNotToBeInSuccess() {
+        expect.not.toBeIn('a', ['b', 'c', 'd']);
+    }
+}
