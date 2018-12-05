3f83e774095e4874e33a1755d71e2cf21fbc21b7
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2018-12-05 12:48:43 -0500

    Implemented base tests visitor decorator

---
+++ src/lib/tests/visitors/decorators/testsVisitorDecorator.ts
@@ -0,0 +1,19 @@
+import { TestsVisitor } from '../testVisitor';
+import { Test } from '../../test';
+import { TestSuite } from '../../testSuite';
+
+export abstract class TestsVisitorDecorator<T> implements TestsVisitor<T> {
+    protected baseVisitTest: (testSuite: Test) => Promise<T>;
+    protected baseVisitTestSuite: (testSuite: TestSuite) => Promise<T>;
+
+    constructor(private baseVisitor: TestsVisitor<T>) {
+        this.baseVisitTest = baseVisitor.visitTest.bind(baseVisitor);
+        this.baseVisitTestSuite = baseVisitor.visitTestSuite.bind(baseVisitor);
+
+        this.baseVisitor.visitTest = this.visitTest.bind(this);
+        this.baseVisitor.visitTestSuite = this.visitTestSuite.bind(this);
+    }
+
+    abstract visitTest(test: Test): Promise<T>;
+    abstract visitTestSuite(test: TestSuite): Promise<T>;
+}
