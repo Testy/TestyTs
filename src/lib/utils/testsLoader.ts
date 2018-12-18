b20a9c74713f7e4aed61d8858aab3f5a7a3c968a
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2018-12-18 17:40:10 -0500

    Extracted methods

--- src/lib/utils/testsLoader.ts
+++ src/lib/utils/testsLoader.ts
@@ -8,13 +8,43 @@ export class TestsLoader {
     private static isTsnodeRegistered = false;
     constructor(private logger?: Logger) { }
 
-    public async loadTests(root: string, patterns: string[], tsconfig: {}): Promise<TestSuite> {
-        // We register the tsnode compiler to transpile the test files
-        if (!TestsLoader.isTsnodeRegistered) {
-            tsnode.register(tsconfig);
-            TestsLoader.isTsnodeRegistered = true;
+    public async loadTests(root: string, patterns: string[], tsconfig: tsnode.Options): Promise<TestSuite> {
+        this.registerTranspiler(tsconfig);
+
+        const testSuites = new TestSuite();
+        testSuites.name = 'Root';
+
+        const files = await this.getTestFiles(root, patterns);
+        for (const file of files) {
+            const testInstances = await this.getTestInstancesFromFile(file));
+            testInstances.forEach(x => testSuites.set(x.name, x));
         }
 
+        return testSuites;
+    }
+
+    private async getTestInstancesFromFile(file: string): Promise<TestSuite[]> {
+        const testSuiteInstances: TestSuite[] = [];
+        const importedFile = await import(file);
+        for (const key in importedFile) {
+            const testSuiteInstance: TestSuite = importedFile[key].__testSuiteInstance;
+            if (!testSuiteInstance)
+                continue;
+
+            testSuiteInstances.push(testSuiteInstance);
+        }
+
+        return testSuiteInstances;
+    }
+
+    private registerTranspiler(tsconfig: tsnode.Options) {
+        if (TestsLoader.isTsnodeRegistered) return;
+
+        tsnode.register(tsconfig);
+        TestsLoader.isTsnodeRegistered = true;
+    }
+
+    private async getTestFiles(root: string, patterns: string[]): Promise<Set<string>> {
         const files: Set<string> = new Set();
         for (const pattern of patterns) {
             const matches = await this.matchFiles(root, pattern);
@@ -23,23 +53,10 @@ export class TestsLoader {
             }
         }
 
-        const testSuites = new TestSuite();
-        testSuites.name = 'Root';
-        for (const file of files) {
-            const importedFile = await import(file);
-            for (const key in importedFile) {
-                const testSuiteInstance: TestSuite = importedFile[key].__testSuiteInstance;
-                if (!testSuiteInstance)
-                    continue;
-
-                testSuites.set(testSuiteInstance.name, testSuiteInstance);
-            }
-        }
-
-        return testSuites;
+        return files;
     }
 
-    private async matchFiles(root: string, pattern: string) {
+    private async matchFiles(root: string, pattern: string): Promise<string[]> {
         return new Promise<string[]>((resolve, reject) => {
             glob(pattern, { cwd: root, ignore: ['node_modules/**'] }, (err, files) => {
                 if (err) reject(err);
