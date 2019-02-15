282f7f55c510b0277e837eb21e867bbc0eca4214
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-02-15 17:39:21 -0500

    Implemented test

---
+++ src/spec/decorators/timeoutDecorator/testSuiteWithTimeouts.ts
@@ -0,0 +1,21 @@
+import { Test } from '../../../testyCore';
+import { TestSuite } from '../../../lib/decorators/testSuite.decorator';
+import { Timeout } from '../../../lib/decorators/timeout.decorator';
+
+@TestSuite()
+export class TestSuiteWithTimeouts {
+
+    @Test()
+    @Timeout(0)
+    private async test1() {
+        return Promise.resolve();
+    }
+
+    @Test()
+    @Timeout(0)
+    private async test2() {
+        return new Promise(res => {
+            setTimeout(() => res(), 100);
+        });
+    }
+}
