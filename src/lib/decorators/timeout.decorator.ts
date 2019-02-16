15b2c0b1f570dcd8d77022508f09f88bf37397bb
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-02-16 09:36:03 -0500

    Added jsdoc

--- src/lib/decorators/timeout.decorator.ts
+++ src/lib/decorators/timeout.decorator.ts
@@ -1,3 +1,7 @@
+/**
+ * Specifies the timeout to cause a test to fail.
+ * @param timeout The timeout in miliseconds.
+ */
 export function Timeout(timeout: number = 2000) {
     return (target, key, descriptor) => {
         target.timeout = timeout;
