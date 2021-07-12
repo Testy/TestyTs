68ae8784e7368d62cfa2b45bea4c4fa13232af21
Antoine Boisier-Michaud <antoine.boisier-michaud@cactusoft.ca>
2021-07-12 07:38:37 -0400

    Fix tests

--- src/spec/assertion/toBeIn.spec.ts
+++ src/spec/assertion/toBeIn.spec.ts
@@ -4,32 +4,32 @@ import { Test, TestSuite } from '../../testyCore';
 @TestSuite('Expect ToBeIn Tests')
 export class ExpectToMatch {
   @Test(`'a' to be in, should succeed`)
-  private aToBeInSuccess() {
+  public aToBeInSuccess() {
     expect.toBeIn('a', ['a', 'b', 'c']);
   }
-  
+
   @Test(`'a' to be in ReadonlyArray, should succeed`)
-  private aToBeInReadonlySuccess() {
-    const readonlyArray: ReaonlyArray<string> = ['a', 'b', 'c'];
+  public aToBeInReadonlySuccess() {
+    const readonlyArray: readonly string[] = ['a', 'b', 'c'];
     expect.toBeIn('a', readonlyArray);
-  }  
+  }
 
   @Test(`'a' to be in, should fail`)
-  private aToBeInFail() {
+  public aToBeInFail() {
     expect.toThrow(() => {
       expect.toBeIn('a', ['b', 'c', 'd']);
     });
   }
 
   @Test(`'a' not to be in, should fail`)
-  private aNotToBeInFail() {
+  public aNotToBeInFail() {
     expect.toThrow(() => {
       expect.not.toBeIn('a', ['a', 'b', 'c']);
     });
   }
 
   @Test(`'a' not to be in, should succeed`)
-  private aNotToBeInSuccess() {
+  public aNotToBeInSuccess() {
     expect.not.toBeIn('a', ['b', 'c', 'd']);
   }
 }
